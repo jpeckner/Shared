04420fe7e452f3f6ff617dd51e344729c172bcdb
Justin Peckner <pecknerj@gmail.com>
2019-11-10 15:10:16 -0800

    Adds SwiftUI version of StyledLabel

--- Shared/UI/Styling/TextLayout.swift
+++ Shared/UI/Styling/TextLayout.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2018 Justin Peckner. All rights reserved.
 //
 
+import SwiftUI
 import UIKit
 
 public struct TextLayout: Equatable {
@@ -17,4 +18,45 @@ public struct TextLayout: Equatable {
         self.font = font
         self.alignment = alignment
     }
+}
+
+@available(iOS 13.0, *)
+public extension Text {
+
+    func applyTextLayout(_ textLayout: TextLayout) -> some View {
+        return
+            font(Font(textLayout.font.ctFont))
+            .multilineTextAlignment(textLayout.alignment.textAlignment)
+    }
+
+    func applyTextColoring(_ textColoring: TextColoring) -> Text {
+        return foregroundColor(Color(textColoring.textColor))
+    }
+
+}
+
+public extension UIFont {
+
+    var ctFont: CTFont {
+        self as CTFont
+    }
+
+}
+
+@available(iOS 13.0, *)
+public extension NSTextAlignment {
+
+    var textAlignment: TextAlignment {
+        switch self {
+        case .left, .natural:
+            return .leading
+        case .center, .justified:
+            return .center
+        case .right:
+            return .trailing
+        @unknown default:
+            fatalError("Unknown NSTextAlignment case: \(self)")
+        }
+    }
+
 }
