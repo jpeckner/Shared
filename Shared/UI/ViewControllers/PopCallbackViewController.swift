e09bb6371996f28c05141bce420aa8893f3b2b13
Justin Peckner <pecknerj@gmail.com>
2020-04-19 10:21:46 -0700

    Imports several re-usable UI components

---
+++ Shared/UI/ViewControllers/PopCallbackViewController.swift
@@ -0,0 +1,13 @@
+//
+//  PopCallbackViewController.swift
+//  Shared
+//
+//  Created by Justin Peckner.
+//  Copyright © 2020 Justin Peckner. All rights reserved.
+//
+
+import Foundation
+
+public protocol PopCallbackViewController {
+    func viewControllerWasPopped()
+}
