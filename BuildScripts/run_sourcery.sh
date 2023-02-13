c5afcdaeaac943d78d3d55ef3eba9caf3edef83c
Justin Peckner <pecknerj@gmail.com>
2023-02-12 16:45:36 -0800

    Cleans-up `run_sourcery.sh`, including removing an unused declaration

--- BuildScripts/run_sourcery.sh
+++ BuildScripts/run_sourcery.sh
@@ -2,18 +2,13 @@
 
 set -e
 
-sourcery                                                \
-  --sources "Shared"                       \
-  --templates "Shared/Sourcery/Templates"  \
+sourcery                                    \
+  --sources "Shared"                        \
+  --templates "Shared/Sourcery/Templates"   \
   --output "Shared/Sourcery/Output"
   
-AUTO_MOCKABLE_IMPORTS="
-import CoreLocation
-import Foundation
-import Shared
-"
-sourcery                                                                \
-  --sources "Shared"                                       \
-  --templates "SharedTestComponents/Sourcery/Templates"    \
-  --args autoMockableImports="CoreLocation",autoMockableImports="Shared"                               \
+sourcery                                                                    \
+  --sources "Shared"                                                        \
+  --templates "SharedTestComponents/Sourcery/Templates"                     \
+  --args autoMockableImports="CoreLocation",autoMockableImports="Shared"    \
   --output "SharedTestComponents/Sourcery/Output"
